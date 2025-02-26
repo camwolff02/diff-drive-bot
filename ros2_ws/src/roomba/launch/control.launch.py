fb4027a26c326165abe36376c96b35528d4f6c9c
camemanuel <crwolff@calpoly.edu>
2025-02-25 18:05:55 -0800

    added run_roomba with params to launch file

--- ros2_ws/src/roomba/launch/control.launch.py
+++ ros2_ws/src/roomba/launch/control.launch.py
@@ -2,22 +2,42 @@ from launch_ros.substitutions import FindPackageShare
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            XMLLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('foxglove_bridge'),
-                    'launch',
-                    'foxglove_bridge_launch.xml'
-                ])
-            ]),
-            launch_arguments={
-                'port': '6006',
-            }.items(),
-        ),
-    ])
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                XMLLaunchDescriptionSource(
+                    [
+                        PathJoinSubstitution(
+                            [
+                                FindPackageShare("foxglove_bridge"),
+                                "launch",
+                                "foxglove_bridge_launch.xml",
+                            ]
+                        )
+                    ]
+                ),
+                launch_arguments={
+                    "port": "6006",
+                }.items(),
+            ),
+            Node(
+                package="roomba",
+                executable="run_roomba",
+                remappings=[("odom", "/gobilda_base_controller/odom")],
+                parameters=[
+                    {"forward_speed": 0.2},
+                    {"turn_speed": 0.1},
+                    {"backup_time": 1.0},
+                    {"stop_dist": 0.3048},
+                    {"obstacle_width": 0.0635},
+                    {"turn_radians": 0.7},
+                ],
+            ),
+        ]
+    )
